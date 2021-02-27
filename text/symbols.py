ec7015d591ec3b06367d9568d12054257ed51d68
zywu <johnson.tsing@gmail.com>
2021-02-28 05:50:18 +0800

    Append EOS token

--- text/symbols.py
+++ text/symbols.py
@@ -7,12 +7,12 @@ The default is a set of ASCII characters that works well for English or text tha
 from text import cmudict
 
 _pad        = '_'
-_punctuation = '!\'(),.:;? '
-_special = '-'
-_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+_eos        = '~'
+_characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!\'\"(),-.:;? %/'
+_digits     = '0123456789'
 
 # Prepend "@" to ARPAbet symbols to ensure uniqueness (some are the same as uppercase letters):
 _arpabet = ['@' + s for s in cmudict.valid_symbols]
 
 # Export all symbols:
-symbols = [_pad] + list(_special) + list(_punctuation) + list(_letters) + _arpabet
+symbols = [_pad, _eos] + list(_characters) + list(_digits) #+ _arpabet
